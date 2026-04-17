06c69680d5bd0da871d6744d16da855f5f1ad629
Surya <suryanarayan0320@gmail.com>
2026-04-18 00:04:52 +0530

    Save

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './index.css'
 // import Post from "./components/Post";
-// import Side from "./components/Side"
-// import RenderStories from "./components/Storiesss"
+import Side from "./components/Side"
+import RenderStories from "./components/Storiesss"
 import RenderStoriesss from "./components/Post"
 
 
 function App(){
   return(
     <>
+      <Side />
+      <RenderStories />
       <RenderStoriesss />
+
     </>
   )
 }
